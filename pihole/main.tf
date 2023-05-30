6ec85e968d8aa3bf37182e2216ddd067e6d13a1c
Przemyslaw Gorzynski <przemyslaw.gorzynski@protonmail.com>
2023-05-30 22:14:12 +0200

    1

--- pihole/main.tf
+++ pihole/main.tf
@@ -15,7 +15,7 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key_path = pathexpand(var.private_key_path)
+  private_key_path = var.private_key_path
   region           = var.region
 }
 
