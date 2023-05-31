7b12f27b8989808ff374a97e0dfa769a499cd003
Przemyslaw Gorzynski <przemyslaw.gorzynski@protonmail.com>
2023-05-31 13:47:56 +0200

    wip

--- pihole/networking.tf
+++ pihole/networking.tf
@@ -49,3 +49,18 @@ protocol                  = "6"
 destination               = "0.0.0.0/0"
 destination_type          = "CIDR_BLOCK"
 }
+
+resource "oci_core_network_security_group_security_rule" "pihole_nsg_ingress" {
+for_each = toset(["22", "80", "443"])
+network_security_group_id = oci_core_network_security_group.pihole_nsg.id
+direction                 = "INGRESS"
+protocol                  = "6"
+source                    = "0.0.0.0/0"
+destination_type          = "CIDR_BLOCK"
+  tcp_options {
+    destination_port_range {
+      min = each.key
+      max = each.key
+    }
+  }
+}
